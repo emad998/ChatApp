446b6e17795338120a85afb5894a699bc7226dcd
Emad <emad998@yahoo.com>
2020-09-13 12:48:44 -0700

    iniital commit

---
+++ client/build/precache-manifest.cfcddce7fe7d81a768e8d7ee0437f652.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa883d158ddb4ff768fc8fe29455c342",
+    "url": "/index.html"
+  },
+  {
+    "revision": "733dafccb2f5698be130",
+    "url": "/static/css/main.5a179674.chunk.css"
+  },
+  {
+    "revision": "9bac09375c13eab8c880",
+    "url": "/static/js/2.7669cad2.chunk.js"
+  },
+  {
+    "revision": "30c40d404319c6bcbaa4f69d872fdf2e",
+    "url": "/static/js/2.7669cad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "733dafccb2f5698be130",
+    "url": "/static/js/main.c8b1f9da.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "d0c57f4110dd0e34c1aaa564d8ca8faa",
+    "url": "/static/media/hey-yall.d0c57f41.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
